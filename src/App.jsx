b672043db52fcf1e6eed4867a3fff55727312020
afounderch <afounder.cherlahealth@protonmail.com>
2025-09-22 14:14:07 +0530

    Edge Code Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import RecipeForm from "./pages/recipe/AddRecipe";  // adjust path
-import ToDoLabels from "./pages/ToDo/ToDoLabelList"; // adjust path
+import ToDoLabels from "./pages/ToDo/TodoLabelList"; // fixed casing in path
 import ToDoLeftRightEdge from "./pages/ToDo/ToDoLeftToToDoRight"; // adjust path casing
 import Supplements from "./pages/Supplements/Supplements"; // adjust path
-import Home from "./pages/ToDo/ToDoLeftToToDoRight"; // new import
 
 
 const App = () => {
@@ -15,10 +14,10 @@ const App = () => {
                 <nav className="bg-gray-800 p-4">
           <ul className="flex space-x-4">
              <li key="todo">
-              <Link to="/DBAdminPanel/todo">ToDo Labels</Link>
+              <Link to="/todo">ToDo Labels</Link>
             </li>
             <li key="todoEdge">
-              <Link to="/DBAdminPanel/todoEdge">ToDo Left/Right Edge Mappings</Link>
+              <Link to="/todoEdge">ToDo Left/Right Edge Mappings</Link>
             </li>
             {/* <li key="recipe">
               <Link to="/DBAdminPanel/recipe">Recipe</Link>
@@ -35,9 +34,9 @@ const App = () => {
           <Route exact path="/DBAdminPanel" element={<h1>Hello</h1>} />
         
           <Route path="/DBAdminPanel/recipe" element={<RecipeForm />} />
-          <Route path="/DBAdminPanel/todo" element={<ToDoLabels />} />
-          <Route path="/DBAdminPanel/supplements" element={<Supplements />} />
-          <Route path="/DBAdminPanel/todoEdge" element={<ToDoLeftRightEdge />} />
+          <Route path="/todo" element={<ToDoLabels />} />
+          <Route path="/supplements" element={<Supplements />} />
+          <Route path="/todoEdge" element={<ToDoLeftRightEdge />} />
         </Routes>
       </div>
     </Router>
